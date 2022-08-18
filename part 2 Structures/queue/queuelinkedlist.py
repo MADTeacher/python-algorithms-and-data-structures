0fd79e826a78891cff0a6e6516adf704510955a0
Stanislav Chernyshev <madmasm@gmail.com>
2022-08-18 22:18:07 +0300

    added queue

--- part 2 Structures/queue/queuelinkedlist.py
+++ part 2 Structures/queue/queuelinkedlist.py
@@ -4,11 +4,7 @@ from typing import TypeVar, Generic, Optional
 T = TypeVar("T")
 
 
-class StackOverFlowException(Exception):
-    pass
-
-
-class EmptyStackException(Exception):
+class EmptyQueueException(Exception):
     pass
 
 
@@ -27,12 +23,12 @@ class SingleNode(Generic[T]):
     next_ptr: Optional['SingleNode[T]'] = None
 
 
-class StackLinkedList(Generic[T]):
+class QueueLinkedList(Generic[T]):
 
-    def __init__(self, size: int) -> None:
+    def __init__(self) -> None:
         self._length: int = 0
-        self.fixed_size: int = size
         self._head: Optional[SingleNode[T]] = None
+        self._tail: Optional[SingleNode[T]] = None
 
     def get_size(self) -> int:
         return self._length
@@ -41,21 +37,20 @@ class StackLinkedList(Generic[T]):
         return self._length == 0
 
     def push(self, value: T):
-        if self._length >= self.fixed_size:
-            raise StackOverFlowException(f"StackOverFlowException: {value}")
         node = SingleNode[T](value, None)
         if self._length == 0:
             self._head = node
+            self._tail = node
             self._length += 1
             return
 
-        node.next_ptr = self._head
-        self._head = node
+        self._tail.next_ptr = node
+        self._tail = node
         self._length += 1
 
     def pop(self) -> T:
         if self.is_empty():
-            raise EmptyStackException("StackOverFlowException")
+            raise EmptyQueueException("EmptyQueueException")
         node = self._head
         self._head = node.next_ptr
         self._length -= 1
@@ -63,10 +58,10 @@ class StackLinkedList(Generic[T]):
 
     def peak(self) -> T:
         if self.is_empty():
-            raise EmptyStackException("Stack is empty")
+            raise EmptyQueueException("Queue is empty")
         return self._head.data
 
-    def print_stack(self) -> None:
+    def print_queue(self) -> None:
         if self.is_empty():
             print("Stack is empty")
         my_list: list[T] = []
@@ -75,28 +70,25 @@ class StackLinkedList(Generic[T]):
         while current_node.next_ptr is not None:
             current_node = current_node.next_ptr
             my_list.append(current_node.data)
-        print(f"Stack: {my_list}")
+        print(f"Queue: {my_list}")
 
 
 if __name__ == '__main__':
-    fix_size: int = 3
-    stack: StackLinkedList[Cat] = StackLinkedList[Cat](fix_size)
-    try:
-        stack.push(Cat("Max", 4))
-        stack.push(Cat("Alex", 5))
-        stack.push(Cat("Tom", 7))
-        stack.print_stack()
-        stack.push(Cat("Tommy", 1))
-    except StackOverFlowException as ex:
-        print(ex)
+    queue: QueueLinkedList[Cat] = QueueLinkedList[Cat]()
+    queue.push(Cat("Max", 4))
+    queue.push(Cat("Alex", 5))
+    queue.push(Cat("Tom", 7))
+    queue.push(Cat("Tommy", 1))
+    queue.print_queue()
 
-    print(f"Stack head value: {stack.peak()}")
+    print(f"Queue head value: {queue.peak()}")
 
     print("----- Pop ------")
+    length = queue.get_size()
     try:
-        for i in range(0, fix_size + 1):
-            pop_value = stack.pop()
-            print(f"Stack head value: {pop_value}")
+        for i in range(0, length + 1):
+            pop_value = queue.pop()
+            print(f"Queue head value: {pop_value}")
 
-    except EmptyStackException as ex:
+    except EmptyQueueException as ex:
         print(ex)
